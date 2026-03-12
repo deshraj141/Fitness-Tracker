4876663d3a0f187e95354cb156f78c89391f3bdb
Deshraj Soni <deshugamer2@gmail.com>
2026-03-12 12:48:52 +0530

    fix wildcard route for production

--- server/server.js
+++ server/server.js
@@ -52,9 +52,9 @@ if (process.env.NODE_ENV === 'production') {
     const path = require('path');
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
-    });
+    app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+});
 }
 
 app.listen(PORT, () => {
